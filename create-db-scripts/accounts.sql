2588f36e3690ba37e75103e217ea983a905ebd3d
Zach <zacharyamassey@gmail.com>
2025-07-31 11:20:30 -0400

    gnosis remote management sql DBA-210

--- create-db-scripts/accounts.sql
+++ create-db-scripts/accounts.sql
@@ -1,13 +1,13 @@
 -- create CRUD application user 
-create user 'gnosisapp'@'%' identified by 'gnosis:app,-logic';
+create user 'gnosisapp'@'%' identified by [readacted];
 grant all privileges on gnosis_management.* to 'gnosisapp'@'%';
 
 -- create phpMyadmin  user for locklear
-create user 'marklocklear'@'%' identified by 'gnosis:eval:abt,-2025';
+create user 'marklocklear'@'%' identified by [redacted];
 grant select on gnosis_management.* to 'marklocklear'@'%';
 
 -- db admin 
-create user 'gnosisdb-admin'@'%' identified by 'Plague:9576,-';
+create user 'gnosisdb-admin'@'%' identified by [redacted];
 grant all privileges on gnosis_management.* to 'gnosisdb-admin'@'%';
 
 
